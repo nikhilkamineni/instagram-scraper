4e082106242b8bc2e38416f6933d986a9b527485
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-10-11 14:29:45 -0700

    Add test for /api/getData/:page endpoint
    (untracked) modified:   .env
    - Add BASE_URL var in .env
    
    modified:   server.test.js
    - Add require's for 'node-fetch' and 'dotenv'
    - Add test for /api/getData/:page endpoint

--- server.test.js
+++ server.test.js
@@ -1,5 +1,6 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
-// const fetch = require('node-fetch');
+const fetch = require('node-fetch');
 const MongodbMemoryServer = require('mongodb-memory-server').MongoMemoryServer;
 // const User = require('./User/UserModel');
 
@@ -22,5 +23,16 @@ describe('Server.js test suite', () => {
   test('Basic test', () => {
     const message = 'Hello World!';
     expect(typeof message).toBe('string');
+  });
+
+  test('GET /api/getData/:page', async () => {
+    const response = await fetch(
+      `${process.env.BASE_URL}/api/getData/cats_of_instagram`
+    );
+    const data = await response.json();
+    expect(response.status).toBe(200);
+    expect(data).toBeDefined();
+    expect(data.page).toBeDefined();
+    expect(Array.isArray(data.posts)).toBe(true);
   });
 });
