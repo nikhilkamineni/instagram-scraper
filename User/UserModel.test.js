e43ae4d6c600b557c1d582c89f05dfa5c4c3c202
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-10-11 16:20:32 -0700

    Add test for UserModel requiring a password

--- User/UserModel.test.js
+++ User/UserModel.test.js
@@ -31,4 +31,19 @@ describe('User.js test suite', () => {
     expect(savedUser.username).toBe('newTestUser');
     expect(savedUser.password).toBe('123456');
   });
+
+  test('UserModel throws error if password is not supplied', async () => {
+    const newUser = { username: 'newTestUser' };
+    let savedUser;
+    let error;
+
+    try {
+      savedUser = await new User(newUser).save();
+    } catch(err) {
+      error = err;
+    }
+
+    expect(savedUser).toBeUndefined();
+    expect(error).toBeDefined();
+  });
 });
