2848d1209f8112121285cd991671d61e683d69f6
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-10-11 22:27:26 -0700

    Added and improved tests for UserModel

--- User/UserModel.test.js
+++ User/UserModel.test.js
@@ -5,12 +5,19 @@ const User = require('./UserModel');
 const mongod = new MongodbMemoryServer();
 
 describe('User.js test suite', () => {
+  let existingTestUser;
   beforeAll(async () => {
     const uri = await mongod.getConnectionString();
     await mongoose.connect(
       uri,
       { useNewUrlParser: true }
     );
+
+    const testUserData = {
+      username: 'existingTestUser',
+      password: '123456'
+    };
+    existingTestUser = await new User(testUserData).save();
   });
 
   afterAll(async () => {
@@ -18,36 +25,63 @@ describe('User.js test suite', () => {
     mongod.stop();
   });
 
+  test('findById retrieves user correctly', async () => {
+    const retrievedUser = await User.findById(existingTestUser._id);
+    expect(retrievedUser).toBeDefined();
+    expect(retrievedUser.username).toBe('existingTestUser');
+    expect(retrievedUser.password).toBe('123456');
+  });
 
-  test('UserModel saves new user correctly', async () => {
+  test('findById returns null if id does not exist', async () => {
+    const randomId = mongoose.Types.ObjectId();
+    const retrievedUser = await User.findById(randomId);
+    expect(retrievedUser).toBeNull();
+  });
+
+  test('findById throws error when receiving invalid id format', async () => {
+    const randomNumber = Math.trunc(Math.random() * 100000 + 1);
+    let retrievedUser;
+    try {
+      retrievedUser = await User.findById(randomNumber);
+      expect(retrievedUser).toBeNull();
+    } catch (error) {
+      expect(error).toBeDefined();
+    }
+    expect(retrievedUser).toBeUndefined();
+  });
+
+  test('Creating a new User works correctly', async () => {
     const newUser = { username: 'newTestUser', password: '123456' };
     const savedUser = await new User(newUser).save();
+    const retrievedUser = await User.findOne(newUser);
+
     expect(savedUser).toBeDefined();
     expect(savedUser.username).toBe('newTestUser');
     expect(savedUser.password).toBe('123456');
+    expect(retrievedUser).toBeDefined();
+    expect(retrievedUser.username).toBe('newTestUser');
+    expect(retrievedUser.password).toBe('123456');
   });
 
-
-  test('UserModel throws error if password is not supplied', async () => {
+  test('Creating a new User throws error if password is not supplied', async () => {
     const newUser = { username: 'newTestUser' };
     let savedUser;
 
     try {
       savedUser = await new User(newUser).save();
-    } catch(error) {
+    } catch (error) {
       expect(savedUser).toBeUndefined();
       expect(error).toBeDefined();
     }
   });
 
-
-  test('UserModel throws error if username is not supplied', async () => {
+  test('Creating a new User throws error if username is not supplied', async () => {
     const newUser = { password: '123456' };
     let savedUser;
 
     try {
       savedUser = await new User(newUser).save();
-    } catch(error) {
+    } catch (error) {
       expect(savedUser).toBeUndefined();
       expect(error).toBeDefined();
     }
