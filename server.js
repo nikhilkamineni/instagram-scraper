50e4daedf66861522c01fe7820656529adf5d977
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-11-01 01:23:00 -0700

    Modify server startup message

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const mongoose = require('mongoose');
 const PORT = process.env.PORT;
 
 const server = app.listen(PORT, () => {
-  console.log(`\n=== Server listening on PORT: ${PORT} ===\n`);
+  console.log('\n========== Instagram-Scraper =========')
+  console.log(`\n=== Server listening on PORT: ${PORT} ===`);
 });
 
 mongoose.connect(
