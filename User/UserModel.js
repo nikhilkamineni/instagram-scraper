b6654a7fae71346da029b7134ce5fd0ef2e0f58e
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-12-10 11:38:42 +0800

    Add pre-save hook and check PW to User model

--- User/UserModel.js
+++ User/UserModel.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const BCRYPT_COST = 11;
 
 const UserSchema = new mongoose.Schema({
   username: {
@@ -18,6 +21,19 @@ const UserSchema = new mongoose.Schema({
   ]
 });
 
-const User = mongoose.model('User', UserSchema);
+UserSchema.pre('save', next => {
+  bcrypt.hash(this.password, BCRYPT_COST, (err, hash) => {
+    if (err) return next(err);
+    this.password = hash;
+    next();
+  });
+});
 
-module.exports = User;
+UserSchema.methods.checkPassword = (passwordAttempt, cb) => {
+  bcrypt.compare(passwordAttempt, this.password, (err, isMatch) => {
+    if (!isMatch) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+module.exports = mongoose.model('User', UserSchema);
