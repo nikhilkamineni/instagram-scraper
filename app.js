7c773c47fa51cb17c4295c0f8db7e029871cd457
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2019-01-20 22:04:19 -0800

    Move get-data route to separate file

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const cheerio = require('cheerio');
 const cors = require('cors');
-const fetch = require('node-fetch');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const path = require('path');
 
 const authRouter = require('./routes/auth');
+const getDataRouter = require('./routes/get-data')
 const userRouter = require('./routes/user');
 const authenticate = require('./middleware/authenticate');
 
@@ -21,53 +20,11 @@ app.use(cors({ origin: '*', credentials: true }));
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use('/api/auth', authRouter);
+app.use('/api/get-data', getDataRouter);
 app.use('/api/user', authenticate, userRouter);
 
 app.get('/api', (req, res) => {
   res.status(200).json({ message: 'STATUS: OK' });
 });
-
-// Get a User's data
-app.get('/api/get-data', async (req, res) => {
-  const handle = req.query.handle;
-
-  try {
-    // Attempt to scrape data
-    const url = `https://www.instagram.com/${handle}/`;
-    const data = await fetch(url);
-    const text = await data.text();
-    const $ = await cheerio.load(text);
-    const allScripts = $('script').toArray();
-
-    let targetScript = await allScripts.find(script => {
-      if (script.children.length) {
-        const data = script.children[0].data;
-        return data.includes('window._sharedData = {');
-      }
-    });
-
-    targetScript = targetScript.children[0].data;
-    let scriptContents = targetScript.substring(
-      targetScript.indexOf('{'),
-      targetScript.length - 1
-    );
-    scriptContents = eval('(' + scriptContents + ')');
-    const postData = scriptContents.entry_data.ProfilePage[0].graphql.user;
-    const name = postData.full_name;
-    const posts = postData.edge_owner_to_timeline_media.edges.map(edge => {
-      return {
-        url: edge.node.display_url,
-        timestamp: edge.node.taken_at_timestamp,
-        dimensions: edge.node.dimensions
-      };
-    });
-
-    res.status(200).json({ name, handle, posts });
-  } catch (err) {
-    // Failed to scrape data
-    res.status(500).json({ message: 'Error fetching!', err });
-    console.error(err); // eslint-disable-line
-  }
-}); // /api/getData
 
 module.exports = app;
---
+++ routes/get-data.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const cheerio = require('cheerio');
+const fetch = require('node-fetch');
+
+const getDataRouter = express.Router();
+
+// Get a User's data
+getDataRouter.get('/', async (req, res) => {
+  const handle = req.query.handle;
+
+  try {
+    // Attempt to scrape data
+    const url = `https://www.instagram.com/${handle}/`;
+    const data = await fetch(url);
+    const text = await data.text();
+    const $ = await cheerio.load(text);
+    const allScripts = $('script').toArray();
+
+    let targetScript = await allScripts.find(script => {
+      if (script.children.length) {
+        const data = script.children[0].data;
+        return data.includes('window._sharedData = {');
+      }
+    });
+
+    targetScript = targetScript.children[0].data;
+    let scriptContents = targetScript.substring(
+      targetScript.indexOf('{'),
+      targetScript.length - 1
+    );
+    scriptContents = eval('(' + scriptContents + ')');
+    const postData = scriptContents.entry_data.ProfilePage[0].graphql.user;
+    const name = postData.full_name;
+    const posts = postData.edge_owner_to_timeline_media.edges.map(edge => {
+      return {
+        url: edge.node.display_url,
+        timestamp: edge.node.taken_at_timestamp,
+        dimensions: edge.node.dimensions
+      };
+    });
+
+    res.status(200).json({ name, handle, posts });
+  } catch (err) {
+    // Failed to scrape data
+    res.status(500).json({ message: 'Error fetching!', err });
+    console.error(err); // eslint-disable-line
+  }
+}); // /api/getData
+
+module.exports = getDataRouter;
